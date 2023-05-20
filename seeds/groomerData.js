85d06adec18fcdaa44c56dcee38af77fe155b7d2
csullins <camilesullins@gmail.com>
2023-05-20 16:24:58 -0500

    post JS design mods

--- seeds/groomerData.js
+++ seeds/groomerData.js
@@ -7,97 +7,96 @@ const groomerdata =
   
     "name":"Jack",
     "profilePicture": "profile1.png",
-    "email": "Jack@gmail.co,",
+    "email": "Jack@gmail.com",
     "phone":"512112456",
-    "motto":"likes pet",
+    "motto":"Life is a canvas, dogs are the art!",
     
   },
   {
     "name":"Simon",
     "profilePicture": "profile2.png",
-     "email": "Simon@gmail.co,",
+     "email": "Simon@gmail.com",
     "phone":"5123344545",
-    "motto":"likes pet",
+    "motto":"Carpe diem that groom!",
 
   },
   {
     "name":"Chirst",
     "profilePicture": "profile3.png",
-    "email": "christ@gmail.co,",
+    "email": "christ@gmail.com",
     "phone":"5127286741",
-    "motto":"happy pet",
+    "motto":"Simon says, give me your dog!",
 
   },
   {
     "name":"Jony",
     "profilePicture": "profile4.png",
-    "email": "jony@gmail.co,",
+    "email": "jony@gmail.com",
     "phone":"512112456",
-    "motto":"likes pet",
+    "motto":"Dogs are truly the inspiration for everything I do!",
       },
   {
     "name":"Jay",
     "profilePicture": "profile5.png",
-    "email": "jay@gmail.co,",
+    "email": "jay@gmail.com",
     "phone":"512112456",
-    "motto":"likes pet",
+    "motto":"If your dog's in need of some glamour, I'm the groomer for you.",
 
   },
   {
     "name":"Fami",
     "profilePicture": "profile6.png",
-    "email": "Fami@gmail.co,",
+    "email": "Fami@gmail.com",
     "phone":"512112456",
-    "motto":"likes pet",
+    "motto":"Dog's best friend",
   
   },
   {
     "name":"Gigi",
     "profilePicture": "profile7.png",
-     "email": "gigi@gmail.co,",
+     "email": "gigi@gmail.com",
     "phone":"5121113234",
-    "motto":"likes pet",
+    "motto":"The limit does not exist!",
 
   },
   {
     "name":"Han",
     "profilePicture": "profile8.png",
-     "email": "han@gmail.co,",
+     "email": "han@gmail.com",
     "phone":"512112456",
-    "motto":"likes pet",
+    "motto":"Luke, I am your real father.",
 
   },
   {
     "name":"Nency",
     "profilePicture": "profile9.png",
-    "email": "Nency@gmail.co,",
+    "email": "Nency@gmail.com",
     "phone":"5128896754",
-    "motto":"likes pet",
+    "motto":"Beauty is in the eyes of the be-howl-der.",
 
   },
   {
     "name":"Jay",
     "profilePicture": "profile10.png",
-     "email": "jay@gmail.co,",
+     "email": "jay@gmail.com",
     "phone":"5121123423",
-    "motto":"likes pet",
+    "motto":"Whenever you have a grooming emergency, my doors are always open.",
 
   },
   {
     "name":"Khyti",
     "profilePicture": "profile11.png",
-     "email": "khyti@gmail.co,",
+     "email": "khyti@gmail.com",
     "phone":"512112456",
-    "motto":"likes pet",
+    "motto":"No pet too difficult, no groom too tough for Khyti.",
 
   },
   {
     "name":"Lalo",
     "profilePicture": "profile12.png",
-     "email": "lalo@gmail.co,",
+     "email": "lalo@gmail.com",
     "phone":"512112456",
-    "motto":"likes pet",
-
+    "motto":"Likes pet",
   },
  
 
